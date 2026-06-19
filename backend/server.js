2cf27e4819e62f9481fafe887d22d4ed0674c8e9
WhoTFTookShadowFlare <therealyaboipug@gmail.com>
2026-06-19 10:41:31 -0600

    Added GET target 'workspaces/:id'

--- backend/server.js
+++ backend/server.js
@@ -183,6 +183,19 @@ app.get('/workspaces', async (req, res) => {
   }
 });
 
+app.get('/workspaces/:id', async (req, res) => {
+  try {
+    const db = readDB();
+	
+	const result = db.workspaces.filter(workspace => workspace._id === req.params.id);
+	if(result.length !== 1) return res.status(404).json({ message: "Workspace not found" });
+	return res.status(200).json(result[0]);
+
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+});
+
 // ── EDIT WORKSPACE ────────────────────────────────────────────────────────────
 app.put('/workspaces/:id', authenticateToken, async (req, res) => {
   try {
